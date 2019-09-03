f6897c8be2588f852a5630f9acdb754d0a75c9c9
Josue Ramon Zavala Soto <josuez-c@herbalife.com>
2019-09-03 14:32:59 -0500

    Removing show and hide dropdown (showmenu variable and methods removed)

--- shop.mobile-ordering.front/src/components/SearchInput.js
+++ shop.mobile-ordering.front/src/components/SearchInput.js
@@ -7,39 +7,9 @@ class SearchInput extends React.Component{
     super(props);
 
     this.state = {
-      showMenu: false,
       inputValue: ''
     }
 
-      this.showMenu = this.showMenu.bind(this);
-      this.closeMenu = this.closeMenu.bind(this);
-
-      this.handleLoginKeyUp = this.keyUpHandler.bind(this, 'LoginInput');
-      this.handlePwdKeyUp = this.keyUpHandler.bind(this, 'PwdInput');
-
-
-  }
-
-  keyUpHandler(refName, e) {
-        console.log(refName.props.value);
-        // prints either LoginInput or PwdInput
-    }
-
-  showMenu(event) {
-    event.preventDefault();
-    this.setState({ showMenu: true }, () => {
-     document.addEventListener('click', this.closeMenu);
-   });
-  }
-
-  closeMenu(event) {
-    if (!this.dropdownMenu.contains(event.target)) {
-
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener('click', this.closeMenu);
-      });
-
-    }
   }
 
   updateInputValue(evt) {
@@ -54,27 +24,17 @@ render (){
         <div className="App">
           <header className="App-header">
             <div className="dropdown">
-                <button onClick={this.showMenu} className="dropbtn">Find Locale...</button>
-
-                    {
-                  this.state.showMenu
-                    ? (
-                       <input
-                          type="text"
-                          placeholder="Search.."
-                          id="myInput"
-                          ref={(element) => {
-                            this.dropdownMenu = element;
-                          }}
-                          value={this.state.inputValue}
-                          onChange={evt => this.updateInputValue(evt)}
-                        ></input>
-                    )
-                    : (
-                      null
-                    )
-                }
-
+                <button className="dropbtn">Find Locale...</button>
+                <input
+                   type="text"
+                   placeholder="Search.."
+                   id="myInput"
+                   ref={(element) => {
+                     this.dropdownMenu = element;
+                   }}
+                   value={this.state.inputValue}
+                   onChange={evt => this.updateInputValue(evt)}
+                 ></input>
               </div>
           </header>
 
